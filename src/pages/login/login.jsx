f0584a56f9f0f8816156daae456ee67f4e959142
KevinDevelopment <kevincristhian@outlook.com>
2023-03-03 21:25:15 -0300

    :sparkles: feat: show message from api in screen

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./index.css";
 
-import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { Form, Label, Input, Button } from "reactstrap";
 
 import logo from "../../images/Colorido 1.png";
 
 export default function Login() {
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState();
+  const [errors, setErrors] = useState();
 
-  function alerta() {
-    alert("esta funcionando")
+  function sendUserData() {
+    fetch("http://localhost:8080/signin", {
+      method: "POST",
+      body: JSON.stringify({
+        email: email,
+        password: password
+      }),
+      headers: { "Content-type": "application/json;charset=UTF-8" }
+    })
+      .then(response => response.json())
+      .then(json => {
+        setErrors(json.message)
+      })
+      .catch(err => console.log(err))
   }
+
+  console.log(errors)
 
   return (
     <div className="imageLogin" style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }} >
@@ -24,27 +41,35 @@ export default function Login() {
           <Form className="row g-3">
 
             <div className="col-md-12">
-              <Label>Email</Label>
-              <Input type="email" placeholder="email@email.com" />
+              <Label>Email:</Label>
+              <Input type="email" placeholder="email@email.com" required onChange={(e) => [setEmail(e.target.value), setErrors(" ")]} />
             </div>
 
             <div className="col-md-12">
-              <Label>Senha</Label>
-              <Input type="password" placeholder="password" />
+              <Label>Senha:</Label>
+              <Input type="password" placeholder="password" onChange={(e) => [setPassword(e.target.value), setErrors(" ")]} />
             </div>
 
+            {
+              errors === "Successfully logged in" ?
+                <div style={{ textAlign: "center", color: "green" }}>{errors}</div> :
+                <div style={{ textAlign: "center", color: "red" }}>{errors}</div>
+
+            }
+
             <div style={{ textAlign: "center" }}>
-              <Button color="danger" onClick={alerta} className="col-md-11" >Login</Button>
+              <Button color="danger" className="col-md-11" onClick={sendUserData}>Login</Button>
 
               <div>não possui cadastro ? registre-se abaixo</div>
               <hr />
-              <Link to="/register"><Button color="danger" className="col-md-11">Registre-se</Button></Link>
+              <Link to="/register"><Button color="danger" className="col-md-11" >Registre-se</Button></Link>
             </div>
 
           </Form>
         </div>
 
       </div>
+      <div style={{ color: "white", fontSize: 12, marginTop: 7 }}>&reg; Hoxtak Aeroespacial. &trade; Todos os diretos reservados</div>
     </div>
   );
 }
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,55 +1,35 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { Form, div, Label, Input, Button } from "reactstrap";
 
 import logo from "../../images/Colorido 1.png";
 
 export default function Register() {
 
-  const [id, setId] = useState();
   const [name, setName] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [errors, setErrors] = useState();
 
-
-  function sendData(e) {
-    e.preventDefault()
-
-    if (!id) {
-      document.getElementById("userId").style.border = "2px solid red"
-      return
-    } else if (!name) {
-      document.getElementById("userName").style.border = "2px solid red"
-      return
-    } else if (!email) {
-      document.getElementById("userEmail").style.border = "2px solid red"
-      return
-    } else if (!password) {
-      document.getElementById("userPassword").style.border = "2px solid red"
-      return
-    } else {
-      sendUserData();
-    }
-
-  }
 
   function sendUserData() {
     fetch("http://localhost:8080/user", {
       method: "POST",
       body: JSON.stringify({
-        id: id,
         name: name,
         email: email,
         password: password
       }),
       headers: { "Content-type": "application/json;charset=UTF-8" }
     })
-    .then(response => response.json())
-    .then(json => (console.log(json)))
-    .catch(err => console.log(err))
+      .then(response => response.json())
+      .then(json => {
+        setErrors(json.message);
+      })
+      .catch(err => console.log(err))
   }
-  
+
 
 
   return (
@@ -61,30 +41,34 @@ export default function Register() {
         </div>
 
         <div className="card-body">
-          <Form className="column g-3" onSubmit={sendData} method="POST" action="http://localhost:8080/user">
+          <Form className="column g-3" id="form">
 
-            <FormGroup className="col-md-12">
-              <Label>Numero de identificação:</Label>
-              <Input type="number" placeholder="Digite seu ID aqui" id="userId" onBlur={(e) => setId(e.target.value)} />
-            </FormGroup>
-
-            <FormGroup className="col-md-12">
+            <div className="col-md-12 required">
               <Label>Nome:</Label>
-              <Input type="text" placeholder="Digite seu nome" id="userName" onBlur={(e) => setName(e.target.value)} />
-            </FormGroup>
+              <Input type="text" placeholder="Digite seu nome" id="userName" required onChange={(e) => [setName(e.target.value), setErrors(" ")]} />
+            </div>
 
-            <FormGroup className="col-md-12">
+            <div className="col-md-12">
               <Label>Email</Label>
-              <Input type="email" placeholder="email@email.com" id="userEmail" onBlur={(e) => setEmail(e.target.value)} />
-            </FormGroup>
+              <Input type="email" placeholder="email@email.com" id="userEmail" required onChange={(e) => [setEmail(e.target.value), setErrors(" ")]} />
+            </div>
 
-            <FormGroup className="col-md-12">
+            <div className="col-md-12 mb-3">
               <Label>Senha:</Label>
-              <Input type="password" placeholder="password" id="userPassword" onBlur={(e) => setPassword(e.target.value)} />
-            </FormGroup>
+              <Input type="password" placeholder="password" id="userPassword" required onChange={(e) => [setPassword(e.target.value), setErrors(" ")]} />
+            </div>
+
+            <div style={{ marginBottom: 10 }}>
+              {
+                errors === "Successfully registered user" ?
+                  <div style={{ textAlign: "center", color: "green" }}>{errors}</div> :
+                  <div style={{ textAlign: "center", color: "red" }}>{errors}</div>
+
+              }
+            </div>
 
             <Link to="/"><Button color="danger">Login</Button></Link>
-            <Button color="danger" className="ms-4" type="submit">Registre-se</Button>
+            <Button color="danger" className="ms-4" onClick={sendUserData}>Registre-se</Button>
 
           </Form>
         </div>
